f1e028db1de89d7b94def1bef8c94b11e1299755
islam elhady <i.elhady44@gmail.com>
2024-04-29 07:46:37 +0300

    check if user logedin or not

--- app/src/main/java/com/elhady/superstore/ui/common/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/elhady/superstore/ui/common/viewmodel/UserViewModel.kt
@@ -5,14 +5,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.elhady.superstore.data.datastore.AppPreferencesDataSource
-import com.elhady.superstore.data.repository.UserPreferenceRepository
-import com.elhady.superstore.data.repository.UserPreferenceRepositoryImpl
+import com.elhady.superstore.data.repository.user.UserPreferenceRepository
+import com.elhady.superstore.data.repository.user.UserPreferenceRepositoryImpl
 import com.elhady.superstore.data.repository.common.AppDataStoreRepositoryImpl
 import com.elhady.superstore.data.repository.common.AppPreferenceRepository
+import com.elhady.superstore.data.repository.user.UserFireStoreRepository
+import com.elhady.superstore.data.repository.user.UserFireStoreRepositoryImpl
 
 class UserViewModel(
     private val userPreferencesRepository: UserPreferenceRepository,
-    private val appPreferenceRepository: AppPreferenceRepository
+    private val appPreferenceRepository: AppPreferenceRepository,
+    private val userFireStoreRepository: UserFireStoreRepository
 ) : ViewModel() {
 
     suspend fun saveLoginState(isLoggedIn: Boolean) {
@@ -28,10 +31,11 @@ class UserViewModelFactory(
 
     private val appPreferenceRepository = AppDataStoreRepositoryImpl(AppPreferencesDataSource(context))
     private val userPreferencesRepository = UserPreferenceRepositoryImpl(context)
+    private val userFireStoreRepository = UserFireStoreRepositoryImpl()
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
         if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST") return UserViewModel(
-                userPreferencesRepository, appPreferenceRepository
+                userPreferencesRepository, appPreferenceRepository, userFireStoreRepository
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
